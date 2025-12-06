ab8b4d20ab26056fefd4e4f34b3390e3d5134cbe
ALBERTO CARREON CARREON <carreonalberto920@gmail.com>
2025-12-06 04:06:08 -0600

    Implementacion de rutas del menu principal

--- src/JS/getIdMedico.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-
-    const id_medico = localStorage.getItem("id_medico");
-    document.getElementById("idMedico").textConten = id_medico;
-
-});
